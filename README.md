341ccf7b01ad97640fe61816468bfbf9ec63cf78
Su Wang <suw@uci.edu>
2013-09-19 16:38:54 -0700

    Fix setup script to check if bashrc exists - Also create .vim/ so the link works

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 #suw's dotfiles
 My common dotfiles, use at your own discretion.
-
-## Todo list
-* Clean up setup script to make move original files if they exist
--- setup.sh
+++ setup.sh
@@ -5,10 +5,24 @@
 # manually transferred
 # ======
 
+# Go to the home dir
 cd ~
+
+# Move existing bashrc if it exists. The other files usually do not exist
+if [ ! -f .bashrc ]; then
+   mv .bashrc .bashrc.old
+fi
+
+# Make .vim directory for link if it doesn't exist
+if [ ! -d .vim ]; then
+    mkdir .vim/
+fi
+
+
+# Link everything
 ln -s dev/dotfiles/.tmux.conf .tmux.conf
 ln -s dev/dotfiles/.bashrc .bashrc
 ln -s dev/dotfiles/.vimrc .vimrc
 ln -s dev/dotfiles/.vim/ .vim/
 
-print 'Done linking';
+echo 'Done linking';
